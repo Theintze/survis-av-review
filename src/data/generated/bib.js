237aedd5cc0f678d447f864031790978d8b2e24a
Ekmel Intze <36741001+Theintze@users.noreply.github.com>
2026-05-09 06:44:40 +0100

    Uploaded directly from PC

---
+++ src/data/generated/bib.js
@@ -0,0 +1,72 @@
+﻿define({ entries : {
+    "ai_vehicle_security_advanced": {
+        "keywords": "autonomous vehicles, artificial intelligence, anomaly detection, cybersecurity, defence",
+        "note": "Accessed: 2026-05-09",
+        "title": "Enhancing Autonomous Vehicle Security Through Advanced Artificial Intelligence Techniques",
+        "type": "misc",
+        "url": "https://upubscience.com/jcsee/article/view/894"
+    },
+    "av_cps_security": {
+        "keywords": "autonomous vehicles, cyber-physical systems, CPS security, system security, attack surface",
+        "note": "Accessed: 2026-05-09",
+        "title": "Security of Autonomous Vehicle as a Cyber-Physical System",
+        "type": "misc",
+        "url": "https://ieeexplore.ieee.org/document/8303906"
+    },
+    "av_security_by_design": {
+        "keywords": "autonomous vehicles, security by design, secure architecture, design principles, cybersecurity",
+        "note": "Accessed: 2026-05-09",
+        "title": "Autonomous Vehicle: Security by Design",
+        "type": "misc",
+        "url": "https://ieeexplore.ieee.org/document/9130160"
+    },
+    "av_security_conceptual_model": {
+        "keywords": "autonomous vehicles, conceptual model, security architecture, modelling, cybersecurity",
+        "note": "Accessed: 2026-05-09",
+        "title": "Autonomous Vehicle Security: Conceptual Model",
+        "type": "misc",
+        "url": "https://ieeexplore.ieee.org/document/8903691"
+    },
+    "av_security_hybrid_threat_model": {
+        "keywords": "autonomous vehicles, threat modelling, hybrid threat model, cybersecurity, risk assessment",
+        "note": "Accessed: 2026-05-09",
+        "title": "Autonomous Vehicle Security: Hybrid Threat Modeling Approach",
+        "type": "misc",
+        "url": "https://ieeexplore.ieee.org/document/11039067"
+    },
+    "av_security_taxonomy": {
+        "keywords": "autonomous vehicles, taxonomy, attacks, defences, threat landscape",
+        "note": "Accessed: 2026-05-09",
+        "title": "Autonomous Vehicle Security: A Taxonomy of Attacks and Defences",
+        "type": "misc",
+        "url": "https://ieeexplore.ieee.org/document/7917080"
+    },
+    "blockchain_ai_av_security": {
+        "keywords": "autonomous vehicles, blockchain, artificial intelligence, security challenges, defence strategies",
+        "note": "Accessed: 2026-05-09",
+        "title": "Autonomous Vehicles Security: Challenges and Solutions Using Blockchain and Artificial Intelligence",
+        "type": "misc",
+        "url": "https://ieeexplore.ieee.org/document/10023964"
+    },
+    "in_vehicle_networking_iov_security": {
+        "keywords": "in-vehicle networking, Internet of Vehicles, IoV, CAN bus, autonomous vehicles",
+        "note": "Accessed: 2026-05-09",
+        "title": "In-Vehicle Networking/Autonomous Vehicle Security for Internet of Things/Vehicles",
+        "type": "misc",
+        "url": "https://www.mdpi.com/2079-9292/10/6/637"
+    },
+    "integrating_av_safety_security": {
+        "keywords": "autonomous vehicles, safety, security, systems integration, risk",
+        "note": "Accessed: 2026-05-09",
+        "title": "Integrating Autonomous Vehicle Safety and Security",
+        "type": "misc",
+        "url": "https://ieeexplore.ieee.org/document/10763275"
+    },
+    "ioav_security_overview": {
+        "keywords": "autonomous vehicles, V2X, communications security, overview, literature review",
+        "note": "Accessed: 2026-05-09",
+        "title": "Internet of Autonomous Vehicles Communications Security: Overview, Issues, and Directions",
+        "type": "misc",
+        "url": "https://ieeexplore.ieee.org/document/8809661"
+    }
+}});
